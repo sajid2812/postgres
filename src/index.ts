b4299fb4d2f673ed279ffefde4f77fe19fba6732
sajid-edninja <ssk73573@gmail.com>
2025-03-22 21:16:49 +0530

    update user

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@ const pgCLient = new Client({
   port: 5432,
   host: "ep-sparkling-sunset-a12q7qcb-pooler.ap-southeast-1.aws.neon.tech",
   database: "neondb",
-  ssl:true
+  ssl: true,
 });
 
 async function connectDB() {
   await pgCLient.connect();
-  const response = await pgCLient.query("SELECT * FROM users;");
+  await pgCLient.query("UPDATE users SET username='jiman' WHERE id=1");
+  const response = await pgCLient.query("SELECT * FROM users");
   console.log(response.rows);
 }
 
