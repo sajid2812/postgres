b29d0715162ae730d183621ef96483fa5b418f04
sajid-edninja <ssk73573@gmail.com>
2025-03-25 09:38:02 +0530

    add transaction

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,11 @@ app.post("/signup", async (req, res) => {
   const { username, password, email, city, country, street, pincode } =
     req.body;
   try {
-    const insertQuery = `INSERT INTO users {username, email, password } VALUES {$1, $2, $3} RETURNING id`;
+    const insertQuery = `INSERT INTO users {username, email, password } VALUES {$1, $2, $3} RETURNING id;`;
+    const addressInsertQuery = `INSERT INTO address {city, country, street, pincode, user_id} values {$1, $2, $3, $4, $5};`;
+    await pgCLient.query("BEGIN;");
     const user = await pgCLient.query(insertQuery, [username, email, password]);
     const user_id = user.rows[0].id;
-    const addressInsertQuery = `INSERT INTO address {city, country, street, pincode, user_id} values {$1, $2, $3, $4, $5}`;
     await pgCLient.query(addressInsertQuery, [
       city,
       country,
@@ -36,6 +37,7 @@ app.post("/signup", async (req, res) => {
       pincode,
       user_id,
     ]);
+    await pgCLient.query("COMMIT;");
     res.status(200).json({ message: "You have signed up" });
   } catch (e) {
     console.log(e);
