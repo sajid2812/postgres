8790908f38011134e4d1ab2de06fa8391d8abb32
sajid-edninja <ssk73573@gmail.com>
2025-03-25 09:17:46 +0530

    add signup route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { Client } from "pg";
+import express from "express";
 
 const pgCLient = new Client({
   user: process.env.NEONDB_OWNER,
@@ -10,16 +11,35 @@ const pgCLient = new Client({
   ssl: true,
 });
 
+const app = express();
+
+app.use(express.json());
+
 async function connectDB() {
   await pgCLient.connect();
-  const insertQuery = `INSERT INTO users {username, email, password} VALUES {$1, $2, $3}`;
-  const response = await pgCLient.query(insertQuery, [
-    "Sajid",
-    "sajid@gmail.com",
-    "12345",
-  ]);
-  const users = await pgCLient.query("SELECT * FROM users");
-  console.log(users.rows);
 }
 
 connectDB();
+
+app.post("/signup", async (req, res) => {
+  const { username, password, email, city, country, street, pincode } =
+    req.body;
+  try {
+    const insertQuery = `INSERT INTO users {username, email, password, email, city, country, street, pincode} VALUES {$1, $2, $3, $4, $5, $6, $7}`;
+    await pgCLient.query(insertQuery, [
+      username,
+      password,
+      email,
+      city,
+      country,
+      street,
+      pincode,
+    ]);
+    res.status(200).json({ message: "You have signed up" });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({
+      message: "Error while signing up",
+    });
+  }
+});
