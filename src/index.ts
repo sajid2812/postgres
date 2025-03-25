812078d8ae3cd47f40d3718cb51a6eb05e09c607
sajid-edninja <ssk73573@gmail.com>
2025-03-25 09:24:55 +0530

    add address entry

--- src/index.ts
+++ src/index.ts
@@ -25,15 +25,16 @@ app.post("/signup", async (req, res) => {
   const { username, password, email, city, country, street, pincode } =
     req.body;
   try {
-    const insertQuery = `INSERT INTO users {username, email, password, email, city, country, street, pincode} VALUES {$1, $2, $3, $4, $5, $6, $7}`;
-    await pgCLient.query(insertQuery, [
-      username,
-      password,
-      email,
+    const insertQuery = `INSERT INTO users {username, email, password } VALUES {$1, $2, $3} RETURNING id`;
+    const user = await pgCLient.query(insertQuery, [username, email, password]);
+    const user_id = user.rows[0].id;
+    const addressInsertQuery = `INSERT INTO address {city, country, street, pincode, user_id} values {$1, $2, $3, $4, $5}`;
+    await pgCLient.query(addressInsertQuery, [
       city,
       country,
       street,
       pincode,
+      user_id,
     ]);
     res.status(200).json({ message: "You have signed up" });
   } catch (e) {
